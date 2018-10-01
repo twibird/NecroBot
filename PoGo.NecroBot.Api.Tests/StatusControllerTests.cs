12278fca39f3fd8deb8eb0360d0f77c26eb36103
Zachariah Grummons <zgrummons@gmail.com>
2018-10-01 14:16:07 -0500

    Endpoint health test

---
+++ PoGo.NecroBot.Api.Tests/StatusControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PoGo.NecroBot.Api.Tests
+{
+    [TestClass]
+    public class StatusControllerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            HttpClient client = new HttpClient();
+            
+            Assert.AreEqual(client.GetAsync(new Uri("localhost/status")).Result.StatusCode, HttpResponseCode.OK);
+        }
+    }
+}
