dddb0a0eb2e87b81dac2c4c1511bdf40b2c93766
Zachariah Grummons <zgrummons@gmail.com>
2018-10-01 14:08:58 -0500

    test project boilerplate

---
+++ PoGo.NecroBot.Api.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PoGo.NecroBot.Api.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("DUNCMP01")]
+[assembly: AssemblyProduct("PoGo.NecroBot.Api.Tests")]
+[assembly: AssemblyCopyright("Copyright © DUNCMP01 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b72fd6bd-d7d6-4608-b456-e415eb5600c8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
