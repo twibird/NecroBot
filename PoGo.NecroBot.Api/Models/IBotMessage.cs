307cc1acaee9990d43ccffa4a4055342d4a8f5e3
Zachariah Grummons <zgrummons@gmail.com>
2018-10-29 09:39:26 -0500

    Implemented basic messaging functionality

---
+++ PoGo.NecroBot.Api/Models/IBotMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PoGo.NecroBot.Api.Models
+{
+    interface IBotMessage
+    {
+        void SetText(string);
+    }
+}
