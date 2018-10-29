15f9b1abf74cc180911d04b4ef985e03559e3c5f
Zachariah Grummons <zgrummons@gmail.com>
2018-10-29 09:32:27 -0500

    Bot comunication scaffolding

---
+++ PoGo.NecroBot.Api/Models/BotCommunicate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PoGo.NecroBot.Api.Models
+{
+    public class BotCommunicate : IBotCommunicate
+    {
+        public void Send(string command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
